dc528ea63de49f0726dad25f6304b8213cc080d1
Lifeache <1163456098@qq.com>
2018-08-26 14:10:33 +0800

    更新

---
+++ OnlyGame/app/src/main/java/com/lifeache/gamecore/Attack.java
@@ -0,0 +1,21 @@
+package com.lifeache.gamecore;
+import com.lifeache.og.*;
+
+public class Attack
+{
+	/**
+	*攻击结算。
+	*
+	*/
+	//发起攻击的单位
+	Unit attacker;
+	//目标单位
+	Unit target;
+
+	public Attack(Unit attacker, Unit target)
+	{
+		this.attacker = attacker;
+		this.target = target;
+	}
+	
+}
---
+++ OnlyGame/app/src/main/java/com/lifeache/gamecore/Damage.java
@@ -0,0 +1,24 @@
+package com.lifeache.gamecore;
+
+import com.lifeache.og.*;
+
+public class Damage
+{
+	/**
+	*伤害。可以产生生命值的变化。
+	*/
+	//伤害来源
+	Unit sourceUnit;
+	//伤害值
+	int damage;
+
+	public Damage(Unit sourceUnit)
+	{
+		this.sourceUnit = sourceUnit;
+	}
+
+	public Unit getSourceUnit()
+	{
+		return sourceUnit;
+	}
+}
